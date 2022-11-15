7c9f8b6c06acd617ce5104c0dbd32e1d61fa225f
Petes-Pizza <117421349+Petes-Pizza@users.noreply.github.com>
2022-11-15 15:43:39 -0600

    Add files via upload

--- pizza.js
+++ pizza.js
@@ -1,36 +1,34 @@
 class Pizza {
     constructor() {
-      this.sauce = {name:"Marinara", price: 0};
-      this.size = {name:"Medium", price: 9.99};
+      this.sauce = {name: "Marinara", price: 0};
+      this.size = {name: "Medium", price: 9.99};
       this.cheeses = {
-        "Mozzarella": {side:"Whole", price:[0, .49], extra:false},
-        "Cheddar": {side:"None", price:[.49, .99], extra:false},
-        "Parmesan": {side:"None", price:[.49, .99], extra:false},
+        "Mozzarella": {side: "Whole", price:[0, .49], extra: false},
+        "Cheddar": {side: "None", price:[.49, .99], extra: false},
+        "Parmesan": {side: "None", price:[.49, .99], extra: false},
       }
       this.toppings = {
-        "Sausage": {side:"None", price:[.49, .99], extra:false},
-        "Peperoni": {side:"None", price:[.49, .99], extra:false},
-        "Bell Peppers": {side:"None", price:[0, 0], extra:false},
-        "Chicken": {side:"None", price:[.49, .99], extra:false},
-        "Onions": {side:"None", price:[0, 0], extra:false},
-        "Pineapple": {side:"None", price:[0, 0], extra:false},
-        "Canadian Bacon": {side:"None", price:[.49, .99], extra:false},
-        "Spinach": {side:"None", price:[0, 0], extra:false},
-        "Mushrooms": {side:"None", price:[0, 0], extra:false},
+        "Sausage": {side: "None", price:[.49, .99], extra: false},
+        "Peperoni": {side: "None", price:[.49, .99], extra: false},
+        "Bell Peppers": {side: "None", price:[0, 0], extra: false},
+        "Chicken": {side: "None", price:[.49, .99], extra: false},
+        "Onions": {side: "None", price:[0, 0], extra: false},
+        "Pineapple": {side: "None", price:[0, 0], extra: false},
+        "Canadian Bacon": {side: "None", price:[.49, .99], extra: false},
+        "Spinach": {side: "None", price:[0, 0], extra: false},
+        "Mushrooms": {side: "None", price:[0, 0], extra: false},
       };
       this.price = 9.99;
     }
     calculatePrice() {
-        var total = this.size.price + this.sauce.price;
+        this.price = this.size.price + this.sauce.price;
         for (var i = 0; i < Object.keys(this.cheeses).length; i++) {
-            total += this.cheeses[Object.keys(this.cheeses)[i]].side !== "None" * 
+            this.price += this.cheeses[Object.keys(this.cheeses)[i]].side !== "None" * 
             this.cheeses[Object.keys(this.cheeses)[i]].price[this.cheeses[Object.keys(this.cheeses)[i]].extra];
         }
         for (var i = 0; i < Object.keys(this.toppings).length; i++) {
-            total += this.toppings[Object.keys(this.toppings)[i]].side !== "None" * 
+            this.price += this.toppings[Object.keys(this.toppings)[i]].side !== "None" * 
             this.toppings[Object.keys(this.toppings)[i]].price[this.toppings[Object.keys(this.toppings)[i]].extra];
-        }
-        this.price = total;
     }
     setSize(size, price) {
         this.size = {name:size, price};
